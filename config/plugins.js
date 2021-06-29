1cfe5993c7c53958b1c4e24edf9de753a70c8152
Danielru <danielru@MacBook-Pro-de-Daniel.local>
2021-06-29 18:15:06 -0500

    segund-deploy

--- config/plugins.js
+++ config/plugins.js
@@ -10,3 +10,18 @@ module.exports = ({ env }) => ({
     },
     // ...
   });
+
+  module.exports = ({ env }) => ({
+  
+    graphql: {
+      endpoint: '/graphql',
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 7,
+      amountLimit: 100,
+      introspection: true,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  });
